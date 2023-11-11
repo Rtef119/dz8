d4413e525777b12817ff7d30af9a21e1bf92e0d0
rtef119 <rtef119@gmail.com>
2023-11-11 09:21:13 +0100

    dz8

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="timing.log")
+def example_function():
+ time.sleep(2)
 
 def timing(func, *args, **kwargs):
     start_time = time.time()
@@ -7,3 +12,9 @@ def timing(func, *args, **kwargs):
     execution_time = end_time - start_time
     return result, execution_time
     print(result)
+    logging.info(f"Функція '{func.__name__}' виконана за{execution_time}секунд")
+
+result, execution_time = timing(example_function)
+
+print(f"Результат: {result}")
+print(f"Час виконання: {execution_time} секунд")
